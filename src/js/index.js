79dfa3d926791b1f5a790932e483c7d7c4fdc3f2
saravana <saravanaa.k@zohocorp.com>
2024-01-29 11:14:03 +0530

    commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,25 @@
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg';
 
+var onBtnClick = function (t, opts) {
+  console.log('Someone clicked the button');
+};
+
 window.TrelloPowerUp.initialize({
-  'card-back-section': function(t, options){
-    return {
-      title: 'My Card Back Section',
-      icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-      content: {
-        type: 'iframe',
-        url: t.signUrl('./section.html'),
-        height: 230, // Max height is 1500.
-      },
-      action: {
-        text: 'My Action',
-        callback: (t) => t.popup("https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg"),
-      }
-    };
+  'card-buttons': function (t, opts) {
+    return [{
+      // usually you will provide a callback function to be run on button click
+      // we recommend that you use a popup on click generally
+      icon: GRAY_ICON, // don't use a colored icon here
+      text: 'Open Popup',
+      callback: onBtnClick,
+      condition: 'edit'
+    }, {
+      // but of course, you could also just kick off to a url if that's your thing
+      icon: GRAY_ICON,
+      text: 'Just a URL',
+      condition: 'always',
+      url: 'https://developer.atlassian.com/cloud/trello',
+      target: 'Trello Developer Site' // optional target for above url
+    }];
   }
 });
